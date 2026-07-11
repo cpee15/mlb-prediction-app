3b5b4179911afc586bd545f79f35c8ef185ac7cc
msantoria <michaelsantoria@gmail.com>
2026-07-11 13:30:21 -0500

    Route dashboard to report builder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ import DailyOddsPage from './pages/DailyOddsPage'
 import Bet105SportsbookPage from './pages/Bet105SportsbookPage'
 import ModelProjectionsPage from './pages/ModelProjectionsPage'
 import NewsPage from './pages/NewsPage'
-import MyDashboardWorkbenchPage from './pages/MyDashboardWorkbenchPage'
+import MyDashboardReportBuilderPage from './pages/MyDashboardReportBuilderPage'
 import ModelTrackerPage from './pages/ModelTrackerPage'
 
 // Set VITE_ENABLE_BATTER_PAGE=true in Railway env vars to re-enable the Batter routes.
@@ -79,7 +79,7 @@ export default function App() {
             <Route path="/news" element={<NewsPage />} />
             <Route path="/models/projections" element={<ModelProjectionsPage />} />
             <Route path="/model-tracker" element={<ModelTrackerPage />} />
-            <Route path="/my-dashboard" element={<MyDashboardWorkbenchPage />} />
+            <Route path="/my-dashboard" element={<MyDashboardReportBuilderPage />} />
             <Route path="/matchup/:game_pk" element={<MatchupRoute />} />
             <Route path="/matchup/:game_pk/competitive" element={<CompetitiveAnalysisPage />} />
             <Route path="/standings" element={<StandingsPage />} />
